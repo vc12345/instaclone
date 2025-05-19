42bed207989e83a82ff2195aa5a7b752a804085b
Vic Chu <vchung80@yahoo.com>
2025-05-19 16:11:23 +0100

    Moved image close button

--- app/page.js
+++ app/page.js
@@ -370,7 +370,7 @@ export default function Home() {
                         fileInputRef.current.value = "";
                       }
                     }}
-                    className="absolute top-2 right-2 bg-white rounded-full p-1 shadow-md"
+                    className="absolute top-2 left-2 bg-white rounded-full p-1 shadow-md"
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
