314ff7c618efdc64f7fa35da9018f6bbb41f619d
Vic Chu <vchung80@yahoo.com>
2025-05-19 13:35:37 +0100

    Added redirect to homepage upon log out

--- components/Header.js
+++ components/Header.js
@@ -240,7 +240,7 @@ export default function Header() {
                     </Link>
                     <button
                       onClick={() => {
-                        signOut();
+                        signOut({ callbackUrl: '/' });
                         setShowMenu(false);
                       }}
                       className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
